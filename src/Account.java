27c1c88371fbe1f2b89457a21035f2576a68ca2b
KT <thun.kristian@gmail.com>
2023-09-18 14:01:56 +0200

    added account class and tested it in main

---
+++ src/Account.java
@@ -0,0 +1,50 @@
+public class Account {
+    private double balance;
+    private String name;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public Account(){
+        this.balance = 0;
+    }
+
+    public Account(String name, double balance){
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public boolean whithdraw(double amount){
+        if (amount <= balance && amount > 0){
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deposit(double amount){
+        if (amount > 0 ){
+            balance += amount;
+            return true;
+        }
+        return false;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String toString(){
+        return (name +" har " + balance + "kr på kontot.");
+    }
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Account a1 = new Account();
+        Account a2 = new Account("Kerstin", 200 );
+        Account a3 = new Account(240);
+        a1.setName("Olle");
+        a1.deposit(1000);
+        a1.whithdraw(200);
+        System.out.println(a1);
+        System.out.println(a2);
+        a3.deposit(2000);
+        System.out.println(a3);
     }
 }
